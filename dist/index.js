3119e96f524d8b3f2334e43b5f3072b1ceb37be8
iamlaobie <iamlaobie@gmail.com>
2018-11-22 18:06:46 +0800

    add babel dist

---
+++ dist/index.js
@@ -0,0 +1,89 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _mysql = require('mysql');
+
+var _mysql2 = _interopRequireDefault(_mysql);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const getFirst = rows => {
+  if (rows.length > 0) {
+    const row = rows[0];
+    const key = Object.keys(row)[0];
+    return row[key];
+  }
+  throw new Error(`NO DATA:${undefined.sql}`);
+};
+
+const getFirstRow = rows => rows[0] || {};
+
+const createPool = options => {
+  const pool = _mysql2.default.createPool(options);
+  pool.on('connection', connection => {
+    connection.query('SET SESSION time_zone =\'+8:00\'');
+    connection.query('SET NAMES utf8');
+  });
+
+  const query = (sql, args) => new Promise((resolve, reject) => {
+    pool.query(sql, args, (err, rows) => {
+      if (err) reject(err);else resolve(rows);
+    });
+  });
+
+  const getConnection = () => new Promise((resolve, reject) => {
+    pool.getConnection((err, conn) => {
+      if (err) reject(err);else resolve(conn);
+    });
+  });
+
+  const queryOne = (sql, args) => {
+    const self = { sql };
+    return query(sql, args).then(getFirst.bind(self));
+  };
+
+  const queryObject = (sql, args) => query(sql, args).then(getFirstRow);
+
+  const executeInsert = (sql, args) => query(sql, args).then(result => result.insertId);
+
+  const executeUpdate = (sql, args) => query(sql, args).then(result => result.affectedRows > 0);
+
+  const transaction = sqls => getConnection().then(conn => {
+    const transQuery = (sql, args) => new Promise((resolve, reject) => {
+      conn.query(sql, args, (err, rows) => {
+        if (err) reject(err);else resolve(rows);
+      });
+    });
+    return transQuery('START TRANSACTION').then(() => {
+      const ps = [];
+      sqls.forEach(sql => {
+        ps.push(transQuery(...sql));
+      });
+      return Promise.all(ps).then(() => {
+        transQuery('COMMIT');
+        conn.release();
+        return Promise.resolve(true);
+      }).catch(err => {
+        transQuery('ROLLBACK');
+        conn.release();
+        return Promise.reject(err);
+      });
+    });
+  }).catch(err => Promise.reject(err));
+
+  return {
+    query,
+    queryOne,
+    queryObject,
+    executeInsert,
+    executeUpdate,
+    getConnection,
+    transaction
+  };
+};
+
+exports.default = { createPool };
+
